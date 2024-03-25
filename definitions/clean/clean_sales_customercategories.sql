be280d846687981683d236bf651c55548e5a4982
rosan.tekin@forefront.se <rosan.tekin@forefront.se>
2024-03-25 14:15:59 +0000

    Changed datatype on column valid_from and valid_to

--- definitions/clean/clean_sales_customercategories.sql
+++ definitions/clean/clean_sales_customercategories.sql
@@ -8,8 +8,8 @@ with
             cast(customercategoryid as string) as customer_category_id,
             cast(customercategoryname as string) as customer_category_name,
             cast(lasteditedby as string) as last_edited_by,
-            cast(validfrom as string) as valid_from,
-            cast(validto as string) as valid_to,
+            cast(validfrom as datetime) as valid_from,
+            cast(validto as datetime) as valid_to,
 
         from source
 
